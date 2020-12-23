b61ec577a81ef54520543760bc02ab00cc5d4a2f
NV7150 <7150macNV777DAI@gmail.com>
2020-12-23 15:04:59 +0900

    add ピン系のライブラリ

---
+++ Pins.h
@@ -0,0 +1,13 @@
+//
+// Created by NV7150 on 2020/12/23.
+//
+
+#ifndef TOFU2020TORCHBEARER_PINS_H
+#define TOFU2020TORCHBEARER_PINS_H
+
+#include "Pin.h"
+namespace Pins{
+    const Pin* MOTOR_R = new Pin(1, ANALOG);
+}
+
+#endif //TOFU2020TORCHBEARER_PINS_H
